c8143687ef88cc9eb32994bb3c581811f5cb5da3
Altuğ Buğra Çelenk <bugracelenk01@gmail.com>
2022-11-20 14:47:44 +0300

    added content to about page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,26 +1,23 @@
 import { FC } from 'react';
 import Styles from './About.module.css';
+import { Link } from 'react-router-dom';
 
 const About: FC = () => {
   return (
     <div className={Styles.aboutWrapper}>
       <h1>About</h1>
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-        voluptates, quod, quia, voluptate quae voluptatem quidem quos
-        necessitatibus voluptatibus quibusdam nesciunt. Quisquam, quae
-        voluptates. Quisquam, quae voluptates. Quisquam, quae voluptates.
-      </p>
-
-      <br />
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-        voluptates, quod, quia, voluptate quae voluptatem quidem quos
-        necessitatibus voluptatibus quibusdam nesciunt. Quisquam, quae
-        voluptates. Quisquam, quae voluptates. Quisquam, quae voluptates.
-      </p>
+      <div className={Styles.paragraphs}>
+        <p>
+          Altuğ Buğra is a self taught full stack developer based in Turkey with
+          a passion for building digital services/stuff he wants. He has a
+          background in web development and has worked on a variety of projects
+          as a freelancer. He is currently studying Computer Engineering at
+          Suleyman Demirel University while working/developing at{' '}
+          <Link to="/">T-Fashion</Link>
+          &nbsp;as backend developer.
+        </p>
+      </div>
     </div>
   );
 };
